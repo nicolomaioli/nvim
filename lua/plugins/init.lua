bc6549809ca387d03a9109b8e622e8a579825f48
Nicolo Maioli <nicolomaioli@gmail.com>
2022-08-04 22:19:59 +0100

    Use luasnip

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -56,8 +56,8 @@ return require('packer').startup(function(use)
     use {
         'hrsh7th/nvim-cmp',
         requires = {
-            {'hrsh7th/vim-vsnip'},
-            {'hrsh7th/cmp-vsnip', after = 'nvim-cmp'},
+            {'L3MON4D3/LuaSnip'},
+            {'saadparwaiz1/cmp_luasnip', after = 'nvim-cmp'},
             {'hrsh7th/cmp-nvim-lsp', after = 'nvim-cmp'},
             {'hrsh7th/cmp-path', after = 'nvim-cmp'},
             {'hrsh7th/cmp-buffer', after = 'nvim-cmp'},
