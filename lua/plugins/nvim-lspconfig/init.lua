ac6ca6650b956c4621fbc941318e7e7116ec2bbe
Nicolo Maioli <nicolomaioli@gmail.com>
2022-08-05 09:01:23 +0100

    Wrap diagnostics in function call

--- lua/plugins/nvim-lspconfig/init.lua
+++ lua/plugins/nvim-lspconfig/init.lua
@@ -19,16 +19,22 @@ for k, v in pairs(M) do
     lspconfig[k].setup(v)
 end
 
--- Diagnostics
-vim.lsp.handlers['textDocument/publishDiagnostics'] =
-    vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics,
-        {virtual_text = false, signs = true})
+local load_diagnostics = function()
+    -- Diagnostics
+    vim.lsp.handlers['textDocument/publishDiagnostics'] =
+        vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics,
+            {virtual_text = false, signs = true})
 
-vim.fn.sign_define('LspDiagnosticsSignError',
-    {text = '', texthl = 'LspDiagnosticsDefaultError'})
-vim.fn.sign_define('LspDiagnosticsSignWarning',
-    {text = '', texthl = 'LspDiagnosticsDefaultWarning'})
-vim.fn.sign_define('LspDiagnosticsSignInformation',
-    {text = '', texthl = 'LspDiagnosticsDefaultInformation'})
-vim.fn.sign_define('LspDiagnosticsSignHint',
-    {text = '', texthl = 'LspDiagnosticsDefaultHint'})
+    vim.fn.sign_define('LspDiagnosticsSignError',
+        {text = '', texthl = 'LspDiagnosticsDefaultError'})
+    vim.fn.sign_define('LspDiagnosticsSignWarning',
+        {text = '', texthl = 'LspDiagnosticsDefaultWarning'})
+    vim.fn.sign_define('LspDiagnosticsSignInformation', {
+        text = '',
+        texthl = 'LspDiagnosticsDefaultInformation'
+    })
+    vim.fn.sign_define('LspDiagnosticsSignHint',
+        {text = '', texthl = 'LspDiagnosticsDefaultHint'})
+end
+
+load_diagnostics()
