0f94c8b6b6280d36cada4d946ffe3e5f45d71d46
Nicolo Maioli <nicolomaioli@gmail.com>
2022-08-07 10:02:26 +0100

    Format cmp menu

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,7 +1,13 @@
 local cmp = require('cmp')
+local lspkind = require('lspkind')
 
 if (cmp == nil) then
     print('Could not load nvim-cmp')
+    return
+end
+
+if (lspkind == nil) then
+    print('Could not load lspkind-nvim')
     return
 end
 
@@ -9,6 +15,18 @@ cmp.setup({
     snippet = {
         expand = function(args)
             require('luasnip').lsp_expand(args.body)
+        end
+    },
+    formatting = {
+        fields = {'kind', 'abbr', 'menu'},
+        format = function(entry, vim_item)
+            lspkind.cmp_format({mode = 'symbol_text', maxwidth = 50})(entry,
+                vim_item)
+            local strings = vim.split(lspkind.kind, '%s', {trimempty = true})
+            lspkind.kind = ' ' .. strings[1] .. ' '
+            lspkind.menu = '    (' .. strings[2] .. ')'
+
+            return lspkind
         end
     },
     window = {completion = true, documentation = true},
@@ -19,7 +37,7 @@ cmp.setup({
         ['<C-e>'] = cmp.mapping.abort(),
         ['<CR>'] = cmp.mapping.confirm({select = true})
     }),
-    sources = {{name = 'nvim_lsp'}}
+    sources = {{name = 'nvim_lsp'}, {name = 'buffer'}}
 })
 
 cmp.setup.cmdline('/', {
