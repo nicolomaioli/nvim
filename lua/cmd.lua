b22a707f45bd42112848c8d79e8983c3ac6874d9
Nicolo Maioli <nicolomaioli@gmail.com>
2022-08-21 10:33:23 +0100

    Only set theme if it has been installed
    Avoid annoying missing colorscheme error

--- lua/cmd.lua
+++ lua/cmd.lua
@@ -1,6 +1,15 @@
 local cmd = vim.cmd
 
-cmd [[filetype plugin on]]
-cmd [[syntax enable]]
-cmd [[silent! helptags ALL]]
-cmd [[colorscheme dracula]]
+cmd('filetype plugin on')
+cmd('syntax enable')
+cmd('silent! helptags ALL')
+
+-- Only load the theme is it's avaialbe
+local colors = vim.fn.getcompletion('', 'color')
+local theme = 'dracula'
+
+for _, v in pairs(colors) do
+    if v == theme then
+        cmd('colorscheme ' .. theme)
+    end
+end
