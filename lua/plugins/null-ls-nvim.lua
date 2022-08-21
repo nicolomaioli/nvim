23f316c9bfb60705a7dec4c08c927f0c01bf3bef
Nicolo Maioli <nicolomaioli@gmail.com>
2022-08-21 18:38:56 +0100

    Add configs for Python and Node

--- lua/plugins/null-ls-nvim.lua
+++ lua/plugins/null-ls-nvim.lua
@@ -15,6 +15,9 @@ null_ls.setup({
         end
     end,
     sources = {
+        null_ls.builtins.code_actions.eslint_d,
+        null_ls.builtins.formatting.prettierd,
+        null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.lua_format.with({
             extra_args = {
                 '--column-limit',
