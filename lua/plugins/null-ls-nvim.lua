b2e449ceca7d2127fad49e52eb46ea987b2be468
Nicolo Maioli <nicolomaioli@gmail.com>
2022-08-22 08:45:19 +0100

    Add ansible-lint

--- lua/plugins/null-ls-nvim.lua
+++ lua/plugins/null-ls-nvim.lua
@@ -18,6 +18,7 @@ null_ls.setup({
         null_ls.builtins.code_actions.eslint_d,
         null_ls.builtins.formatting.prettierd,
         null_ls.builtins.formatting.black,
+        null_ls.builtins.diagnostics.ansiblelint,
         null_ls.builtins.formatting.lua_format.with({
             extra_args = {
                 '--column-limit',
