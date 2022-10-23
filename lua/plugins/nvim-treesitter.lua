5fc62fa3628298a8f999bfd6f31e65fcd328baf8
Nicolo Maioli <nicolomaioli@gmail.com>
2022-10-23 14:33:17 +0100

    Install parsers on-demand
    Closes #2

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
-    ensure_installed = 'all',
-    ignore_install = {'phpdoc'},
+    ensure_installed = {'lua'},
+    auto_install = true,
     highlight = {enable = true},
     playground = {
         enable = true,
