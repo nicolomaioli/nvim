51052a247c4460023d911fe76bd18e92294b463e
Nicolo Maioli <nicolomaioli@gmail.com>
2022-10-24 18:04:43 +0100

    Add neovide config

---
+++ lua/gui.lua
@@ -0,0 +1,4 @@
+if vim.g.neovide then
+    vim.opt.guifont = {'FiraCode Nerd Font', ':h12'}
+    vim.g.neovide_hide_mouse_when_typing = true
+end
--- lua/plugins/onedarkpro-nvim.lua
+++ lua/plugins/onedarkpro-nvim.lua
@@ -1,5 +1,5 @@
 require('onedarkpro').setup({
     filetypes = {all = 'false'},
     styles = {comments = 'italic'},
-    options = {window_unfocused_color = true},
+    options = {window_unfocused_color = true, terminal_colors = true},
 })
