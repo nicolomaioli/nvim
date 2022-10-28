81f1d9d7fdbe3a66ab3a811f32c30b136629761d
Nicolo Maioli <nicolomaioli@gmail.com>
2022-10-28 23:26:57 +0100

    Make nvim-tree narrower

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -4,7 +4,7 @@ require('nvim-tree').setup({
     view = {
         adaptive_size = false,
         centralize_selection = false,
-        width = 30,
+        width = 20,
         hide_root_folder = false,
         side = 'left',
         preserve_window_proportions = false,
