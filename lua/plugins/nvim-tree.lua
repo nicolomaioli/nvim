1004081c8b4835da20e46c904f171b33e26cb5a4
Nicolo Maioli <nicolomaioli@gmail.com>
2022-10-28 22:37:43 +0100

    Tweak nvim-tree

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,4 +1,19 @@
-require('nvim-tree').setup({git = {ignore = false}})
+require('nvim-tree').setup({
+    open_on_setup = true,
+    git = {ignore = false},
+    view = {
+        adaptive_size = false,
+        centralize_selection = false,
+        width = 30,
+        hide_root_folder = false,
+        side = 'left',
+        preserve_window_proportions = false,
+        number = false,
+        relativenumber = false,
+        signcolumn = 'yes',
+        mappings = {custom_only = false},
+    },
+})
 
 local load_mappings = function()
     vim.keymap.set('n', '<leader>kb', ':NvimTreeToggle<CR>')
