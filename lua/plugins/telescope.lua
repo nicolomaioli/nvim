27c5d0eb050d03688c3a195b38b31402f1cbc468
Nicolo Maioli <nicolomaioli@gmail.com>
2022-11-04 16:47:26 +0000

    Add builtins

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -32,11 +32,13 @@ telescope.setup {
 }
 
 local load_mappings = function()
+    vim.keymap.set('n', '<leader>fb', builtin.buffers)
     vim.keymap.set('n', '<leader>ff', builtin.find_files)
     vim.keymap.set('n', '<leader>fg', builtin.live_grep)
-    vim.keymap.set('n', '<leader>fb', builtin.buffers)
     vim.keymap.set('n', '<leader>fh', builtin.help_tags)
     vim.keymap.set('n', '<leader>fk', builtin.keymaps)
+    vim.keymap.set('n', '<leader>fm', builtin.marks)
+    vim.keymap.set('n', '<leader>fr', builtin.reloader)
 end
 
 load_mappings()
