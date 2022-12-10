a734671514573f884c6f9351e84f56b770dcc959
Nicolo Maioli <nicolomaioli@gmail.com>
2022-12-10 14:45:20 +0100

    Default to vertical terminal

--- lua/plugins/toggleterm-nvim.lua
+++ lua/plugins/toggleterm-nvim.lua
@@ -1,1 +1,18 @@
-require('toggleterm').setup({open_mapping = [[<C-`>]]})
+require('toggleterm').setup({
+    size = function(term)
+        if term.direction == 'horizontal' then
+            return vim.o.lines * 0.3
+        elseif term.direction == 'vertical' then
+            return vim.o.columns * 0.4
+        end
+    end,
+    direction = 'vertical',
+    open_mapping = [[<C-`>]],
+})
+
+local load_mappings = function()
+    vim.keymap.set('n', '<c-t>', '<Cmd>exe v:count1 . "ToggleTerm"<CR>',
+        {silent = true})
+end
+
+load_mappings()
