f48142254682e71d57cb130f8d80fa31ee769aee
Nicolo Maioli <nicolomaioli@gmail.com>
2023-01-05 12:33:07 +0000

    Highlight treesitter-context

--- lua/plugins/treesitter-context.lua
+++ lua/plugins/treesitter-context.lua
@@ -4,6 +4,7 @@ return {
     config = function()
         require('treesitter-context').setup()
 
+        vim.api.nvim_set_hl(0, 'TreesitterContext', { bg = 'Black' })
         vim.api
             .nvim_set_hl(0, 'TreesitterContextBottom', { underdotted = true })
     end,
